721742852ceaa6d749d7e682d057c504cd2c2151
Ryou1127 <42777611+hatlsy@users.noreply.github.com>
2022-04-30 14:15:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ fetch('https://v1.hitokoto.cn')
 				.then(response => response.json())
 				.then(data => {
 					const hitokoto = document.querySelector('.bt')
-					const fom = document.querySelector('.form')
+					const fom = document.querySelector('.from')
 					hitokoto.innerText = data.hitokoto
 					fom.innerText = data.from
 				})

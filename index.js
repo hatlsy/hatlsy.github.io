4839c7ecd720d27de7262206b1b1d93f3b4213a9
Ryou1127 <42777611+hatlsy@users.noreply.github.com>
2022-04-30 13:56:08 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+fetch('https://v1.hitokoto.cn')
+				.then(response => response.json())
+				.then(data => {
+					const hitokoto = document.querySelector('.bt')
+					const fom = document.querySelector('.form')
+					hitokoto.innerText = data.hitokoto
+					fom.innerText = data.from
+				})
+				.catch(console.error)

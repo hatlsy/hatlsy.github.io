9e287908dd06b167614df5cc068e74ccbf12bb14
Ryou1127 <42777611+hatlsy@users.noreply.github.com>
2022-06-18 16:21:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+document.querySelector('body').addEventListener('click',(e)=>{
+  fetch('https://v1.hitokoto.cn')
+    .then(response => response.json())
+    .then(data => {
+      const hitokoto = document.querySelector('.bt')
+      const fom = document.querySelector('.from')
+      hitokoto.innerText = data.hitokoto
+      fom.innerText = data.from
+    })
+    .catch(console.error)
+})
 fetch('https://v1.hitokoto.cn')
-				.then(response => response.json())
-				.then(data => {
-					const hitokoto = document.querySelector('.bt')
-					const fom = document.querySelector('.from')
-					hitokoto.innerText = data.hitokoto
-					fom.innerText = data.from
-				})
-				.catch(console.error)
+  .then(response => response.json())
+  .then(data => {
+    const hitokoto = document.querySelector('.bt')
+    const fom = document.querySelector('.from')
+    hitokoto.innerText = data.hitokoto
+    fom.innerText = data.from
+  })
+.catch(console.error)
